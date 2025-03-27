01675f146f339642d516b94bd7bf30b2756a3f8a
Rayancrvg <rayangteixeira@gmail.com>
2025-03-27 21:51:53 +0000

    Desafio Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
-
+ 
 int main() {
     // Variáveis Carta 01
     char estado1[5], codigo1[5], cidade1[50]; // Todas as variáveis strings da Carta 01
     int populacao1, ponto_tur1; // Todas as variáveis inteiras da Carta 01
     float area1, pib1; // Todas as variáveis de Ponto Flutuante da Carta 01
+    float dens_popul1, pib_per1; // Inclui as variáveis do Desafio Aventureiro Carta 01
 
     char estado2[5], codigo2[5], cidade2[50]; // Todas as variáveis strings da Carta 02
     int populacao2, ponto_tur2; // Todas as variáveis inteiras da Carta 02
     float area2, pib2; // Todas as variáveis de Ponto Flutuante da Carta 02
-    
+    float dens_popul2, pib_per2; // Inclui as variáveis do Desafio Aventureiro Carta 02
+
     // Carta 01 // Pega as informações da Carta 01.
     printf("Carta 01:\n"); 
     printf("Digite o Estado: "); // Imprime a solicitação para o scanf
@@ -50,7 +52,12 @@ int main() {
     scanf("%f", &pib2); // Lê o que foi digitado após o printf
     printf("Digite o número de pontos turísticos: "); // Imprime a solicitação para o scanf
     scanf("%d", &ponto_tur2); // Lê o que foi digitado após o printf
-
+    
+    dens_popul1 = (float) populacao1 / area1; // Faz o calculo da Denseidade populacional da carta 01 e armazena na variável
+    dens_popul2 = (float) populacao2 / area2; // Faz o calculo da Denseidade populacional da carta 02 e armazena na variável
+    pib_per1 = (float) (pib1 * 1000000000) / populacao1; // Faz o calculo do PIB per Capita da carta 01 e armazena na variável
+    pib_per2 = (float) (pib2 * 1000000000) / populacao2; // Faz o calculo do PIB per Capita da carta 02 e armazena na variável
+    // Estou multipilcando o pib por 1 bilhão para o calculo dar certo
     printf("\nCarta 1:\n"); 
     printf("Estado: %s\n", estado1); // Imprime o estado armazenado na variável "estado1"
     printf("Código: %s\n", codigo1); // Imprime o código armazenado na variável "codigo1"
@@ -59,6 +66,8 @@ int main() {
     printf("Área: %.2f km²\n", area1); // Imprime a Área armazenada na variável "area1"
     printf("PIB: %.2f bilhões de reais\n", pib1); // Imprime o PIB armazenado na variável "pib1"
     printf("Número de Pontos Turísticos: %d\n", ponto_tur1); // Imprime o número de Pontos Turísticos armazenado na variável "ponto_tur1"
+    printf("Densidade Populacional: %.2f hab/km²\n", dens_popul1);
+    printf("PIB per Capita %.2f reais\n", pib_per1);
 
     printf("\nCarta 2:\n"); 
     printf("Estado: %s\n", estado2); // Imprime o estado armazenado na variável "estado2"
@@ -68,6 +77,7 @@ int main() {
     printf("Área: %.2f km²\n", area2); // Imprime a Área armazenada na variável "area2"
     printf("PIB: %.2f bilhões de reais\n", pib2); // Imprime o PIB armazenado na variável "pib2"
     printf("Número de Pontos Turísticos: %d\n", ponto_tur2); // Imprime o número de Pontos Turísticos armazenado na variável "ponto_tur2"
-
+    printf("Densidade Populacional: %.2f hab/km²\n", dens_popul2);
+    printf("PIB per Capita %.2f reais\n", pib_per2);
 
 }
